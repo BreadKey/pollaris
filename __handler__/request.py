48d3c4d7e7ef941469edcca9d4dbc2cfeaf363e0
2breadkey <2breadkey@gmail.com>
2021-04-13 01:38:54 +0900

    fix get data

--- __handler__/request.py
+++ __handler__/request.py
@@ -2,4 +2,4 @@ import json
 
 
 def getData(event) -> dict:
-    return event.get("queryStringParameters") or json.loads(event.get("body", "{}"))
+    return event.get("queryStringParameters") or json.loads(event.get("body") or "{}")
