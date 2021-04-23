5debf7db0f8f66b5a0e2a00e67a55a80a453bdc8
2breadkey <2breadkey@gmail.com>
2021-04-23 18:07:37 +0900

    fix url

--- client.py
+++ client.py
@@ -3,12 +3,18 @@ import requests
 import json
 from auth.model import *
 
-with open("secret/dev.json") as jsonFile:
+with open("secret/dev.json", 'r') as jsonFile:
     secret = json.load(jsonFile)
 
 HOST = "https://" + secret["domain"] + "/dev"
 
 LAST_TOKENS: dict = {}
+
+with open("local/data.json", 'r') as jsonFile:
+    localData: dict = json.load(jsonFile)
+
+    LAST_TOKENS["accessToken"] = localData.get("accessToken")
+
 
 def signUp(id: str, nickname: str, password: str, role: Role = Role.User):
     response = requests.post(HOST + "/signUp", data=json.dumps(
@@ -23,15 +29,32 @@ def signIn(id: str, password: str):
 
     assert response.status_code == 200, response.status_code
     LAST_TOKENS["accessToken"] = response.json()["accessToken"]
+    localData["accessToken"] = LAST_TOKENS["accessToken"]
+
+    with open("local/data.json", 'w') as jsonFile:
+        jsonFile.write(json.dumps(localData))
+
 
 def page():
-    response = requests.get(HOST + "/poll", headers={"Authorization": "bearer " + LAST_TOKENS.get("accessToken", "")})
+    response = requests.get(
+        HOST + "/poll", headers=__buildAuthHeader())
 
-    print(response.status_code)
-    print(response.json())
+    assert response.status_code == 200, response.json()
+
+
+def getNewIdentityChallenge(userId: str) -> str:
+    response = requests.get(
+        HOST + "/challenge", headers=__buildAuthHeader(), params={"userId": userId})
+
+    assert response.status_code == 200, response.json()
+
+    challenge = response.json()["challenge"]
+
 
 def answer(userId: str, pollId: int, index: int):
-    response = requests.post(HOST + "/answer", headers={"Authorization": "bearer " + LAST_TOKENS.get("accessToken", "")}, data=json.dumps({
+    challenge = getNewIdentityChallenge(userId)
+
+    response = requests.post(HOST + "/answer", headers=__buildAuthHeader(), data=json.dumps({
         "userId": userId,
         "option": {
             "pollId": pollId,
@@ -41,3 +64,23 @@ def answer(userId: str, pollId: int, index: int):
     }))
 
     assert response.status_code == 200, response.json()
+
+
+def requestVerificationCode(userId: str, phoneNumber: str):
+    response = requests.get(HOST + "/auth/verification",
+                            params={"userId": userId, "phoneNumber": phoneNumber}, headers=__buildAuthHeader())
+
+    assert response.status_code == 200, response.json()
+
+
+def verifyIdentity(userId: str, code: str):
+    response = requests.post(HOST + "/auth/verification", headers=__buildAuthHeader(), data=json.dumps({
+        "userId": userId,
+        "code": code
+    }))
+
+    assert response.status_code == 200, response.json()
+
+
+def __buildAuthHeader():
+    return {"Authorization": "bearer " + LAST_TOKENS.get("accessToken", "")}
